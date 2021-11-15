683b158b517d186356f0c77ac03258ac97ea0a6a
Nedislav <nedislav1998@gmail.com>
2021-11-15 11:14:15 +0200

    Added comments and UML Diagram

--- src/main/java/Servlet/GetAndPut.java
+++ src/main/java/Servlet/GetAndPut.java
@@ -7,11 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-
+//URL Pattern for the servlet
 @WebServlet(name = "helloServlet", value = "/servlet")
 public class GetAndPut extends HttpServlet {
 
 
+    //Initializing the servlet
     public void init() {}
 
     @Override
@@ -19,7 +20,7 @@ public class GetAndPut extends HttpServlet {
         String req_method = req.getMethod().trim();
 
         System.out.println(req_method);
-
+        //Checking the request method
         if ("PUT".equals(req_method)){
             doPut(req,resp);
         }
@@ -29,12 +30,16 @@ public class GetAndPut extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //set the content type
+        //Added Allow header for GET Request
         response.addHeader("Allow","GET");
+        //set the content type
         response.setContentType("application/xml");
 
+        //Using the MaNik external library to read the file
         MNFileLib fileRead = new MNFileLib();
+        //Using readFile method do read the file
         String toPrint = fileRead.readFile("D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
+        //Printing the read file
         PrintWriter out = response.getWriter();
         out.println(toPrint);
         out.flush();
@@ -43,18 +48,20 @@ public class GetAndPut extends HttpServlet {
 
 
     public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        //Added Allow header for PUT Request
         response.addHeader("Allow","PUT");
 
+        //Getting the input stream
         InputStream bodyStream = request.getInputStream();
-
-
-        //reading all remaining byte from the input stream
+        //Reading all remaining byte from the input stream
         byte []b = bodyStream.readAllBytes();
         String toWrite = new String(b);
 
+        //Using the MaNik external library to write the file
         MNFileLib fileWrite = new MNFileLib();
+        //Using writeToFile method do read the file
         fileWrite.writeToFile(toWrite, "D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
+        //Printing the written file
         System.out.println(toWrite);
     }
 
