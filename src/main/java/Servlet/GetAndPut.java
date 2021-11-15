3b8769b4aa40c6139e5e57999df5c22b0d9d4381
Nedislav <nedislav1998@gmail.com>
2021-11-15 10:00:02 +0200

    New Push

--- src/main/java/Servlet/GetAndPut.java
+++ src/main/java/Servlet/GetAndPut.java
@@ -4,7 +4,8 @@ package Servlet;
 
 
 
-import MaNik.File.FileRead;
+import MaNik.File.FR;
+
 
 import java.io.*;
 import javax.servlet.ServletException;
@@ -15,7 +16,6 @@ import javax.servlet.annotation.*;
 
 //TODO: Raboti PUT Request prez klienta
 // GET NE raboti prez klienta i browser (HTTP Error 500)
-//
 
 
 @WebServlet(name = "helloServlet", value = "/servlet")
@@ -43,27 +43,11 @@ public class GetAndPut extends HttpServlet {
         response.addHeader("Allow","GET");
         response.setContentType("application/xml");
 
-//        //specify the file name
-//        File file = new File("D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
-//
-//        //read the content of the file
-//        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-//        {
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                //show the content on browser(this is a server response to the client)
-//                PrintWriter out = response.getWriter();
-//                out.println(line);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-        FileRead fileRead = new FileRead();
-        fileRead.readFile("D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
+        FR fileRead = new FR();
+        String toPrint = fileRead.readFile("D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
         PrintWriter out = response.getWriter();
-        out.println(out);
+        out.println(toPrint);
+        out.flush();
 
     }
 
@@ -79,22 +63,7 @@ public class GetAndPut extends HttpServlet {
         byte []b = bodyStream.readAllBytes();
         String toWrite = new String(b);
 
-//        try {
-//            //Save the incoming data to a file
-//            FileWriter putWriter = new FileWriter("D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
-//            putWriter.write(toWrite);
-//
-//            //close the connection
-//            putWriter.close();
-//            System.out.println("Received file from client: \n" + toWrite);
-//            System.out.println("Successfully wrote to the file.");
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-
-
-        FileRead fileRead = new FileRead();
+        FR fileRead = new FR();
         fileRead.writeToFile(toWrite, "D:\\Java\\JavaServletAndClient\\src\\main\\java\\Files\\servletfile.xml");
         System.out.println(toWrite);
     }
